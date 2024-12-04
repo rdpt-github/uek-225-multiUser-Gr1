4bcdd22b408fcaa9fcba1b0ddcd040df29556dfe
Jonas Lüthi <jlu150349@stud.gibb.ch>
2024-12-04 13:25:45 +0100

    Lecki testi 🤪

--- Backend/L-Bank.Testing/BookinRepositoryTest.cs
+++ Backend/L-Bank.Testing/BookinRepositoryTest.cs
@@ -1,28 +1,52 @@
-﻿using L_Bank_W_Backend.DbAccess.Repositories;
-using Microsoft.Extensions.Options;
+﻿using L_Bank_W_Backend.Core.Models;
 using L_Bank_W_Backend.DbAccess;
+using L_Bank_W_Backend.DbAccess.Repositories;
+using Microsoft.Extensions.Options;
 
 public class BookingRepositoryTest
 {
-    private readonly IOptions<DatabaseSettings> _settings;
     private readonly BookingRepository _bookingRepository;
+    private readonly LedgerRepository _ledgerRepository;
+    private readonly IOptions<DatabaseSettings> _settings;
 
     public BookingRepositoryTest()
     {
-        _settings = Options.Create(new DatabaseSettings { ConnectionString = "Server=localhost,1433;User Id=sa;Password=YourPassword123;" });
+        _settings = Options.Create(new DatabaseSettings
+        {
+            ConnectionString = "Server=localhost,1433;Database=l_bank_backend;User Id=sa;Password=YourPassword123;"
+        });
         _bookingRepository = new BookingRepository(_settings);
+        _ledgerRepository = new LedgerRepository(_settings);
     }
 
     [Fact]
     public void Book_InsufficientBalance_ReturnsFalse()
     {
+        var ledger1 = new Ledger
+        {
+            Id = 1,
+            Name = "abc",
+            Balance = 1000000
+        };
+        var ledger2 = new Ledger
+        {
+            Id = 1,
+            Name = "bde",
+            Balance = 1000000
+        };
+        _ledgerRepository.Create(ledger1);
+        _ledgerRepository.Create(ledger2);
+
+        var ledgers = _ledgerRepository.GetAllLedgers().ToList();
+        var ledgerId1 = ledgers[^1].Id;
+        var ledgerId2 = ledgers[^2].Id;
+
+
         // Arrange
-        var sourceLedgerId = 1;
-        var destinationLedgerId = 2;
-        var amount = 100;
+        var amount = 10000001;
 
         // Act
-        var result = _bookingRepository.Book(sourceLedgerId, destinationLedgerId, amount);
+        var result = _bookingRepository.Book(ledgerId1, ledgerId2, amount);
 
         // Assert
         Assert.False(result);
